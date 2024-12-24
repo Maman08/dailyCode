b32c0c21aa9115d1178dee166e0fc94561d8171d
mritunjay singh <a34mritunjaysingh@gmail.com>
2024-12-24 23:37:26 +0530

    Done

--- elevatorProject/models/elevator.py
+++ elevatorProject/models/elevator.py
@@ -26,32 +26,62 @@ class Elevator:
     def addFloorReq(self,requestedFloor):
         if requestedFloor not in self.tagret_floor:
             self.tagret_floor.append(requestedFloor)
-
+    
     def move(self):
-        if self.direction=="up":
-            if self.current_floor<10:
-                self.current_floor+=1
-            elif self.current_floor==10:
-                self.direction="down"
-
-        elif self.direction=="down":
-            if self.current_floor >1:
-                self.current_floor-=1
-            elif self.current_floor==1:
-                self.direction="up"
-
+        if self.direction == "up":
+            if self.current_floor < 10:
+                self.current_floor += 1
+            elif self.current_floor == 10:
+                self.direction = "down"
+        elif self.direction == "down":
+            if self.current_floor > 1:
+                self.current_floor -= 1
+            elif self.current_floor == 1:
+                self.direction = "up"
         elif self.direction == "stopped":
-            if self.tagret_floor:
-                target = self.tagret_floor[0]
+            if self.tagret_floor:  # Check if there are any target floors
+                target = self.tagret_floor[0]  # Get the first target floor
                 if self.current_floor < target:
                     self.current_floor += 1
                     self.direction = "up"
                 elif self.current_floor > target:
                     self.current_floor -= 1
-                    self.direction = "down"  
-                else:
-                    self.target_floors.pop(0) 
-                    self.direction = "stopped"
+                    self.direction = "down"
+                else:  # If the elevator is at the target floor
+                    self.tagret_floor.pop(0)  # Remove the reached target floor
+                    if not self.tagret_floor:  # If there are no more target floors
+                        self.direction = "stopped"  # Stop the elevator
+                    else:
+                        # Continue moving towards the next target
+                        self.direction = "stopped"  
+        
+
+    # def move(self):
+    #     if self.direction=="up":
+    #         if self.current_floor<10:
+    #             self.current_floor+=1
+    #         elif self.current_floor==10:
+    #             self.direction="down"
+
+    #     elif self.direction=="down":
+    #         if self.current_floor >1:
+    #             self.current_floor-=1
+    #         elif self.current_floor==1:
+    #             self.direction="up"
+
+    #     elif self.direction == "stopped":
+    #         if self.tagret_floor:
+    #             target = self.tagret_floor[0]
+    #             if self.current_floor < target:
+    #                 self.current_floor += 1
+    #                 self.direction = "up"
+    #             elif self.current_floor > target:
+    #                 self.current_floor -= 1
+    #                 self.direction = "down"  
+    #             else:
+    #                 self.tagret_floor.pop(0) 
+    #                 self.direction = "stopped"
+    
 
     def __str__(self):
         return f"Elevator {self.id}: Floor {self.current_floor}, Direction {self.direction}, Targets: {self.tagret_floor}"
